c48b7d374e86fdc51f5d73ea8ddafa399aa22710
bbcc33 <biancanboykin@gmail.com>
2024-12-04 18:40:29 -0700

    using interpolation

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   validates :role, inclusion: { in: ROLES }
 
  def name
-   [ first_name, last_name ].compact.join(" ")
+   "#{first_name} #{last_name}"
  end
 
   def set_default_role
